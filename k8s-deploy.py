d58bb3559a69da0242a3de02a189037f9887711e
Newton Pasqualini Filho <newtonpasqualini@gmail.com>
2023-01-26 13:52:56 -0300

    deploy script model

---
+++ k8s-deploy.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+import sys
+from os.path import exists
+import requests
+
+APPNAME = sys.argv[1]
+CONTAINER_IMAGE = sys.argv[2]
+NAMESPACE = sys.argv[3]
+if NAMESPACE != "main":
+    APPNAME = "-".join([APPNAME, NAMESPACE])
+
+APPURL="{}.obvious.com.br".format(APPNAME)
+
+CUSTOM_MANIFEST="./manifests/{}.yml".format(APPNAME)
+if exists(CUSTOM_MANIFEST):
+    MANIFEST=CUSTOM_MANIFEST
+if NAMESPACE != "main" and NAMESPACE != "hml":
+    MANIFEST='./k8s/deployments/dev.yml'
+else:
+    MANIFEST="./k8s/deployments/{}.yml".format(NAMESPACE)
+
+try:
+    REGISTRY = sys.argv[4]
+except:
+    REGISTRY = "obvious"
+with open(MANIFEST, 'r') as file:
+    DEPLOYMENT = file.read()
+
+DEPLOYMENT = DEPLOYMENT.replace('__APPNAME', APPNAME)
+DEPLOYMENT = DEPLOYMENT.replace('__NAMESPACE', NAMESPACE)
+DEPLOYMENT = DEPLOYMENT.replace('__APPURL', APPURL)
+DEPLOYMENT = DEPLOYMENT.replace('__CONTAINER_IMAGE', CONTAINER_IMAGE)
+DEPLOYMENT = DEPLOYMENT.replace('__REGISTRY', REGISTRY)
+
+print(DEPLOYMENT)
+
+# INTEGRANDO PORTAL devops-status.obvious.com.br
+DEVOPS_WORKER_URL = "devops-status.obvious.com.br"
+DEVOPS_WORKER_TOKEN = "teste"
+try:
+    response = requests.post("https://{}/newEvent.php?token={}&appslug={}".format(DEVOPS_WORKER_URL, DEVOPS_WORKER_TOKEN, APPNAME), data = {'message':"BUILD do Projeto na URL: https://{}".format(APPURL)})
+except:
+    print("# Erro ao enviar notificação para {}".format(DEVOPS_WORKER_URL))
+    pass
